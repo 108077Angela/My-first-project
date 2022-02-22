413fab159c465c5881a455fadc4c053e356c9a8d
108077Angela <100200583+108077Angela@users.noreply.github.com>
2022-02-22 19:32:47 +0800

    Create ex5.cpp

---
+++ ex5.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main (void) {
+    int myAge = 16;
+    int myHeight = 157;
+    int myWeight = 45;
+    int mySibilingNumber = 1;
+     printf(" I am %d ",myAge); 
+     printf(" years old  \n");
+     printf(" My height is %d ",myHeight);
+     printf(" cm  \n");
+     printf(" My weight is %d ",myWeight);
+     printf(" kg  \n");
+     printf(" I have %d ",mySibilingNumber);
+     printf(" sister  \n");
+}
