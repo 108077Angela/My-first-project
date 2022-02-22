0efbd7197db07a3b8016413bedadfe5b97c6b5a2
108077Angela <100200583+108077Angela@users.noreply.github.com>
2022-02-22 19:07:12 +0800

    Create ex2.cpp

---
+++ ex2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    printf("Hello World \n");
+    
+    cout <<  "hello selena \n";
+    cout <<  "my height is " << 1 << "m " << 57 << "cm";
+
+    return 0;
+}
