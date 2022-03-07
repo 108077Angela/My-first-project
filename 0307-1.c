1ba11703ecf7096c486efecfbf1858d13181ff1e
108077Angela <100200583+108077Angela@users.noreply.github.com>
2022-03-07 19:33:21 +0800

    Create 0307-1.c

---
+++ 0307-1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (void) {
+     printf(" hello Selena "); 
+     printf(" my height is 1 m 56 cm \n",1,80);
+     printf(" this building is %06d cm \n", 890);
+     printf(" this building is %06d cm \n", 7890);
+     printf(" this building is %06d cm \n", 67890);
+     printf(" this building is %06d cm \n", 567890);
+     printf(" this building is %06d cm \n", 1234567890);
+     printf(" pi = %1.10f\n ", 3.1415926535897932384626433832795);
+}
