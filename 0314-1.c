99656993e3bcf55dd7eea0e95dd77c3008e4b01b
108077Angela <100200583+108077Angela@users.noreply.github.com>
2022-03-14 19:18:01 +0800

    Create 0314-1.c

---
+++ 0314-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, product;
+    a = 11;
+    b = 26;
+
+ 
+    // Calculating product
+    product = a * b;
+
+ 
+    printf("Product = %d", product);
+    
+    return 0;
+}
