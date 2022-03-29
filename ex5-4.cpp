d757284977fa1bef71d9ad4c4453dc74f0bbaccc
108077Angela <100200583+108077Angela@users.noreply.github.com>
2022-03-29 20:06:45 +0800

    Create ex5-4.cpp

---
+++ ex5-4.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf(" please input your age \n");
+    scanf("%d" ,&a);
+    printf(" please input her age \n");
+    scanf("%d" ,&b);
+    if(a > b ){
+        printf("you");
+        
+    }else{
+        printf("she");
+    }
+    return 0;
+}
